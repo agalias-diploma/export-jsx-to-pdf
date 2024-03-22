4848b7670e4d17ed6f6f28ddf92776b8dce20e24
GaliasAndriy <halias.andrii@chnu.edu.ua>
2024-03-22 15:03:28 +0200

    Added header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            Export formatted text to PDF
+        </header>
+    )
+}
+
+export default Header;
